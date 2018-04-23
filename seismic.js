27de75fce9939f6c441fda29bf85e65735e9ccf2
Nicholas Perez <nperez9@mail.greenriver.edu>
2018-04-23 10:40:26 -0700

    Changed the way the station range tool functions. Instead of click and drag its now click and scroll. Changed css to handle stacking of stations and its ranged parts.

--- seismic.js
+++ seismic.js
@@ -8,11 +8,12 @@
 $(function () {
 
     //Station object with a pos: x and y, as well as its distance from the earthquake
-    function Station(name, stationX, stationY, distance) {
+    function Station(name, stationX, stationY, distance, range) {
         this.name = name;
         this.stationX = stationX;
         this.stationY = stationY;
         this.distance = distance;
+        this.range = range;
     }
 
     //Earthquake object with a pos: x and y, as well as its magnitude, or strength
@@ -48,12 +49,12 @@ $(function () {
     let stationPool =[];
     let stationDiv = $('#stationDiv');
     let pos = {x: 0, y: 0};
+    let selected = '';
 
     //circle variables
-    let selectorSize = 1;
+    let selectorSize = 0;
     let line = 1;
-    let isHeldDown = false;
-    let lastMouseMoveX = 0;
+
 
 
 
@@ -76,9 +77,9 @@ $(function () {
         for(let i = 0; i < NUMBER_OF_STATIONS; i++){
             stationPool.push(new Station(
                 'station'+(i+1),
-                getRandomIntInclusive(STATION_X_RANGE)*10,
-                getRandomIntInclusive(STATION_Y_RANGE)*10,
-                0));
+                (getRandomIntInclusive(STATION_X_RANGE)*10)+PADDING_COMPENSATION,
+                (getRandomIntInclusive(STATION_Y_RANGE)*10)+PADDING_COMPENSATION,
+                0,0));
 
             stationPool[i].distance = Math.round(
                 findDistance(
@@ -97,8 +98,9 @@ $(function () {
         //creates and adds click functionality for station divs
         creatStationOnClick(stationPool);
 
-        //creates circle measure on click/drag tool
-        createMouseDownEvents(stationPool);
+        //creates circle measure on wheel movement
+        createCircumferenceEvents(stationPool);
+
 
         console.log(stationPool);
 
@@ -119,14 +121,25 @@ $(function () {
             circleClickIdName = stations[i].name+'-circleClick';
 
             stationDiv.append('<div class="station-style" id="'+stations[i].name+'">' +
-                '<span id="'+circleDistanceIdName+'"></span>'+
-                '<div id="'+distanceLineIdName+'"></div>'+
-                '<div id="'+circleClickIdName+'"></div>'+
+                '<span id="'+circleDistanceIdName+'" class="circleSize" style="top:'+parseInt((stations[i].stationY)+15)+'px;left:'+(stations[i].stationX)+'px;"></span>'+
+                '<div id="'+distanceLineIdName+'" class="line" style="top:'+parseInt((stations[i].stationY)+19)+'px;left:'+(stations[i].stationX)+'px;"></div>'+
+                '<div id="'+circleClickIdName+'" class="circle" style="top:'+parseInt((stations[i].stationY)+19)+'px;left:'+(stations[i].stationX)+'px;"></div>'+
                 '</div>');
 
             $('#'+stations[i].name)
-                .css("left", stations[i].stationX+PADDING_COMPENSATION+'px')
-                .css("top", stations[i].stationY+PADDING_COMPENSATION+'px');
+                .css("top", stations[i].stationY+'px')
+                .css("left", stations[i].stationX+'px');
+        }
+    }
+
+
+    //sets the selected to a station object
+    function setSelected(selectedName, stations) {
+        for (let i = 0 ; i < stations.length ; i++){
+
+            if(selectedName == stations[i].name){
+                return stations[i];
+            }
         }
     }
 
@@ -159,54 +172,73 @@ $(function () {
                 $('#stationName').text(stations[i].name).css('text-transform','capitalize');
                 $('#stationXPos').text(stations[i].stationX);
                 $('#stationYPos').text(stations[i].stationY);
+
+                if(selected !== ''){
+                    $('#'+selected.name).removeClass('selected');
+                }
+
+
+                selected = setSelected($(this).attr('id'), stationPool);
+
+                console.log(selected);
+
+                $('#'+selected.name).addClass('selected');
             });
         }
     }
 
 
     /*
-        Circumference Measuring tool for Stations - Click and drag circle representing distance
+        Circumference Measuring tool for Stations
+            - Select station and scroll up or down representing distance
      */
-    function createMouseDownEvents(stations) {
-        for(let i = 0 ; i< stations.length; i++){
-            $('#'+stations[i].name).mousedown(function () {
-                isHeldDown = true;
-            })
-        }
-    }
 
-    $(document).mouseup(function () {
-        isHeldDown = false;
-    });
+    function createCircumferenceEvents(stations) {
+        let wheelEventNumber = 0;
 
-    $(document).mousemove(function (event) {
-        if(lastMouseMoveX === 0){
-            grow();
+        document.addEventListener('wheel', function (event) {
 
-        } else{
-            if(event.offsetX > lastMouseMoveX){
-                grow();
-                lastMouseMoveX = event.offsetX;
-            }else{
-                shrink();
-                lastMouseMoveX = event.offsetX;
+            let top = 0;
+            let left = 0;
+
+            if(selected !== ''){
+                wheelEventNumber = event.deltaY;
+
+                if(wheelEventNumber > 0){
+                    if(selected.range >= 0 ){
+                        selected.range += 2.5;
+
+                        top = window.getComputedStyle((document.querySelector('#'+selected.name+'-circleClick')), null).getPropertyValue('top').split('px')[0];
+                        left = window.getComputedStyle((document.querySelector('#'+selected.name+'-circleClick')), null).getPropertyValue('left').split('px')[0];
+
+                        document.querySelector('#'+selected.name+'-circleClick').style.width = selected.range+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.height = selected.range+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.top = (Number(top)-1)+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.left = (Number(left)-1)+'px';
+                        // test.style.top
+                    }
+                }
+                else if(wheelEventNumber < 0){
+                    if(selected.range > 0 ){
+                        selected.range -= 2.5;
+
+                        top = window.getComputedStyle((document.querySelector('#'+selected.name+'-circleClick')), null).getPropertyValue('top').split('px')[0];
+                        left = window.getComputedStyle((document.querySelector('#'+selected.name+'-circleClick')), null).getPropertyValue('left').split('px')[0];
+
+                        document.querySelector('#'+selected.name+'-circleClick').style.width = selected.range+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.height = selected.range+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.top = (Number(top)+1)+'px';
+                        document.querySelector('#'+selected.name+'-circleClick').style.left = (Number(left)+1)+'px';
+                    }
+
+                }
+                console.log(selected.range);
+
+                document.querySelector('#'+selected.name+'-circumferenceText').innerHTML = selected.range +'km';
             }
-        }
-        lastX = event.offsetX;
-    });
-
-    function circleGrow() {
-        if(isHeldDown){
-            //increase circle size and adjust position back and up to offset the growth
-            //also enlarge the line
-        }
+        });
     }
 
-    function circleShrink() {
-        if(isHeldDown){
-            //same as grow but the opposite
-        }
-    }
 
     //generates seismic graph for each station
 
@@ -225,5 +257,11 @@ $(function () {
     $('#gridToggle').on('click', function () {
         $('#grid').toggleClass('testGrid');
     });
+
+    function changeSelection(newSelection, stations) {
+        if(selected === ''){
+            selected = newSelection;
+        }
+    }
 
 });
